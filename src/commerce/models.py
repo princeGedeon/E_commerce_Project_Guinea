c95542318ad414bec754c621fae8fbe368e2cad2
princeGedeon <guedjegedeon03@gmail.com>
2022-05-06 09:55:15 +0100

    Ajout de la fonctionnalité de recherche ET DE TRI

--- src/commerce/models.py
+++ src/commerce/models.py
@@ -11,6 +11,7 @@ class Tags_Products(models.Model):
         return self.libelle
 class Categorie(models.Model):
     libelle = models.CharField(max_length=100)
+    is_visible=models.BooleanField(default=False)
 
     def __str__(self):
         return self.libelle
--- src/commerce/views.py
+++ src/commerce/views.py
@@ -3,19 +3,45 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView, DetailView
 
-from commerce.models import Product
+from commerce.models import Product,Categorie
+
 
 
 class ProductListView(ListView):
+
+    context_product=Product.objects.all()
     template_name = "pages/market/list_produits.html"
     paginate_by = 16 # add this
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['produits'] = Product.objects.all()
-        return context
+
+
+
 
     def get_queryset(self):
-        return Product.objects.all()
+        item = self.request.GET.get('recherche_article')
+        sort=self.request.GET.get("sort")
+
+        if item != '' and item is not None:
+            context_product = Product.objects.filter(libelle__icontains=item)
+
+        else:
+            context_product=Product.objects.all()
+
+        if sort != '' and sort is not None:
+            if sort == 'rating':
+                context_product = context_product.order_by('price')
+            elif sort == 'rating2':
+                context_product = context_product.order_by('-price')
+            elif sort=='date':
+                context_product=context_product.order_by("-date")
+
+
+        return context_product
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produits'] =self.get_queryset()
+        context['categories']=Categorie.objects.filter(is_visible=True)
+        return context
 
 class ProductDetalView(DetailView):
     model = Product
