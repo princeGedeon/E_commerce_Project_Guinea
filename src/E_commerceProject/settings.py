9b7d3848ec2d8f4db737d0d089657242d6ab2f5a
princeGedeon <guedjegedeon03@gmail.com>
2022-05-08 09:22:54 +0100

    Implémentation de modification de mot de passe avec confirmation mail

--- src/E_commerceProject/settings.py
+++ src/E_commerceProject/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#SMTP
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_HOST_USER="guedjegedeon03@gmail.com"
+EMAIL_HOST_PASSWORD='uyieteoxkxpxgiuv'
+EMAIL_PORT=587
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth import views
 from django.urls import path
 
 
@@ -12,6 +12,10 @@ urlpatterns = [
     path('login/',loginView,name="login"),
     path('logout/',deconnection,name='logout'),
     path('register/',registerView,name="register"),
-    path("profil_add/",profileRegister,name="profile_form")
+    path("profil_add/",profileRegister,name="profile_form"),
+    path('reset_password', views.PasswordResetView.as_view(template_name="pages/authentification/password_reset.html"),name="reset_password"),
+    path('reset_password_send', views.PasswordResetDoneView.as_view(template_name="pages/authentification/password_reset_sent.html"), name="password_reset_done"),
+    path("reset/<uidb64>/<token>",views.PasswordResetConfirmView.as_view(template_name="pages/authentification/password_reset_form.html"),name="password_reset_confirm"),
+    path('reset_password_complete',views.PasswordResetCompleteView.as_view(template_name="pages/authentification/password_reset_done.html"),name="password_reset_complete")
 
 ]
